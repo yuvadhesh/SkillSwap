71f37fef7e0d7682951649b7cf0f564363d21503
Vishalesh-sekar <vishalesh.sekar@gmail.com>
2026-06-23 10:35:21 +0530

    Deployment ready

--- server/index.cjs
+++ server/index.cjs
@@ -95,7 +95,16 @@ const io = new Server(server, {
   }
 });
 
-app.use(cors());
+// app.use(cors());
+const cors = require('cors');
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://skill-swap-seven-eta.vercel.app/'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
